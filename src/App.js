fecc2e5706d6325e024455ab714b79c1550a751f
dadadon <davianberoni@yahoo.com>
2022-09-15 06:09:36 -0500

    add elements to app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>0</Col>
+      </Row>
+      <Row>
+        <Col>AC</Col>
+        <Col>+/-</Col>
+        <Col>%</Col>
+        <Col>/</Col>
+      </Row>
+      <Row>
+        <Col><Button variant="secondary">7</Button></Col>
+        <Col><Button variant="secondary">8</Button></Col>
+        <Col><Button variant="secondary">9</Button></Col>
+        <Col>*</Col>
+      </Row>
+      <Row>
+        <Col><Button variant="secondary">4</Button></Col>
+        <Col><Button variant="secondary">5</Button></Col>
+        <Col><Button variant="secondary">6</Button></Col>
+        <Col>+</Col>
+      </Row>
+      <Row>
+        <Col><Button variant="secondary">1</Button></Col>
+        <Col><Button variant="secondary">2</Button></Col>
+        <Col><Button variant="secondary">7</Button></Col>
+        <Col>-</Col>
+      </Row>
+      <Row>
+        <Col xs={6}>O</Col>
+        <Col>.</Col>
+        <Col>=</Col>
+      </Row>
+    </Container>
   );
 }
 
