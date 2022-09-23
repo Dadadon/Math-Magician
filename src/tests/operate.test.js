74e8c65cb9f073dd9ce49a01a4d4201374525640
Elfin Git <musongoletracy@gmail.com>
2022-09-23 09:43:31 +0200

    Make changes to tests

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -3,12 +3,15 @@ import operate from "../components/logic/operate";
 test("Testing division operator", () => {
   expect(operate(10, 5, "÷")).toBe("2");
 });
+
 test("Testing subtraction operator", () => {
-  expect(operate(6, 4, "-")).toBe("2");
+  expect(operate(6, 11, "-")).toBe("-5");
 });
+
 test("Testing mupltiplication operator", () => {
   expect(operate(3, 4, "x")).toBe("12");
 });
+
 test("Testing addition operator", () => {
-  expect(operate(10, 5, "+")).toBe("15");
+  expect(operate(10, 0, "+")).toBe("10");
 });
