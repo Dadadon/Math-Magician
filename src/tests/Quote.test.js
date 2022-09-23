4f0a817c5feef9bd168cc2eb142b9459767b0271
dadadon <davianberoni@yahoo.com>
2022-09-22 21:40:03 -0500

    Fix linter errors

--- src/tests/Quote.test.js
+++ src/tests/Quote.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Quote from '../components/pages/Quote';
 
-
 it('renders Quotes correctly', () => {
   const tree = renderer.create(<Quote />);
   expect(tree).toMatchSnapshot();
